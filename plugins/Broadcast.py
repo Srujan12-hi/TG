6f4822c22a6498aed50a7a58821a20dccbd281f3
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2022-05-30 15:48:58 +0530

    Update Broadcast.py

--- plugins/Broadcast.py
+++ plugins/Broadcast.py
@@ -6,7 +6,7 @@ import datetime
 import aiofiles
 import asyncio
 import traceback
-from Configs import Config
+from Database.Configs import Config
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
 broadcast_ids = {}
